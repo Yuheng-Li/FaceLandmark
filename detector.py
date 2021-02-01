2658fb99fecdc4aa7faffcdde5eb76015d8ef3e0
Yuheng-Li <yhnli@ucdavis.edu>
2021-01-31 16:21:38 -0800

    update

---
+++ detector.py
@@ -0,0 +1,143 @@
+import os
+import torch
+import numpy as np
+from data import cfg_mnet, cfg_re50
+from layers.functions.prior_box import PriorBox
+from utils.nms.py_cpu_nms import py_cpu_nms
+import cv2
+from models.retinaface import RetinaFace
+from utils.box_utils import decode, decode_landm
+from PIL import Image 
+
+
+def check_keys(model, pretrained_state_dict):
+    ckpt_keys = set(pretrained_state_dict.keys())
+    model_keys = set(model.state_dict().keys())
+    used_pretrained_keys = model_keys & ckpt_keys
+    unused_pretrained_keys = ckpt_keys - model_keys
+    missing_keys = model_keys - ckpt_keys
+    print('Missing keys:{}'.format(len(missing_keys)))
+    print('Unused checkpoint keys:{}'.format(len(unused_pretrained_keys)))
+    print('Used keys:{}'.format(len(used_pretrained_keys)))
+    assert len(used_pretrained_keys) > 0, 'load NONE from pretrained checkpoint'
+    return True
+
+
+def remove_prefix(state_dict, prefix):
+    ''' Old style model is stored with all names of parameters sharing common prefix 'module.' '''
+    print('remove prefix \'{}\''.format(prefix))
+    f = lambda x: x.split(prefix, 1)[-1] if x.startswith(prefix) else x
+    return {f(key): value for key, value in state_dict.items()}
+
+
+def load_model(model, pretrained_path):
+    print('Loading pretrained model from {}'.format(pretrained_path))
+
+    pretrained_dict = torch.load(pretrained_path,  map_location=lambda storage, loc: storage)
+    if "state_dict" in pretrained_dict.keys():
+        pretrained_dict = remove_prefix(pretrained_dict['state_dict'], 'module.')
+    else:
+        pretrained_dict = remove_prefix(pretrained_dict, 'module.')
+    check_keys(model, pretrained_dict)
+    model.load_state_dict(pretrained_dict, strict=False)
+    return model
+
+
+class Detector():
+    def __init__(self, device, confidence_threshold=0.02, nms_threshold=0.4):
+
+        self.net = RetinaFace(cfg=cfg_re50, phase = 'test')    
+        pretrained_path = './weights/Resnet50_Final.pth'
+        self.net = load_model(self.net, pretrained_path)
+        self.net.eval()
+        self.net = self.net.to(device)
+
+        self.cfg = cfg_re50
+        self.device = device
+        self.confidence_threshold = confidence_threshold
+        self.nms_threshold = nms_threshold
+
+
+    def __call__(self, img):
+        "img should be PIL.Image (RGB), it will return dets (see readme.txt) "
+
+        img = np.array( img )[:,:,::-1] # convert to bgr 
+        img = np.float32(img)
+
+   
+        im_height, im_width, _ = img.shape
+        scale = torch.Tensor([img.shape[1], img.shape[0], img.shape[1], img.shape[0]])
+        img -= (104, 117, 123)
+        img = img.transpose(2, 0, 1)
+        img = torch.from_numpy(img).unsqueeze(0)
+        img = img.to(self.device)
+        scale = scale.to(self.device)
+        
+        with torch.no_grad():
+            loc, conf, landms = self.net(img)  # forward pass
+
+        priorbox = PriorBox(self.cfg, image_size=(im_height, im_width))
+        priors = priorbox.forward()
+        priors = priors.to(self.device)
+        prior_data = priors.data
+        boxes = decode(loc.data.squeeze(0), prior_data, self.cfg['variance'])
+        boxes = boxes * scale
+        boxes = boxes.cpu().numpy()
+        scores = conf.squeeze(0).data.cpu().numpy()[:, 1]
+        landms = decode_landm(landms.data.squeeze(0), prior_data, self.cfg['variance'])
+        scale1 = torch.Tensor([img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                            img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                            img.shape[3], img.shape[2]])
+        scale1 = scale1.to(self.device)
+        landms = landms * scale1 
+        landms = landms.cpu().numpy()
+
+        # ignore low scores
+        inds = np.where(scores > self.confidence_threshold)[0]
+        boxes = boxes[inds]
+        landms = landms[inds]
+        scores = scores[inds]
+
+        # keep top-K before NMS
+        order = scores.argsort()[::-1]
+        boxes = boxes[order]
+        landms = landms[order]
+        scores = scores[order]
+
+        # do NMS
+        dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
+        keep = py_cpu_nms(dets, self.nms_threshold)
+        dets = dets[keep, :]
+        landms = landms[keep]
+
+        dets = np.concatenate((dets, landms), axis=1) # THIS IS OUTPUT FOR EACH IMAGE 
+
+        return dets
+
+
+    def draw(self, img, dets):
+        "img and dets should be input and output of __call__() function"
+
+        img_raw = np.float32(np.array( img )[:,:,::-1])
+
+        for b in dets:
+            text = "{:.4f}".format(b[4])
+            b = list(map(int, b))
+            cv2.rectangle(img_raw, (b[0], b[1]), (b[2], b[3]), (0, 0, 255), 2)
+            cx = b[0]
+            cy = b[1] + 12
+            cv2.putText(img_raw, text, (cx, cy), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255))
+
+            # landms
+            cv2.circle(img_raw, (b[5], b[6]), 1, (0, 0, 255), 4)
+            cv2.circle(img_raw, (b[7], b[8]), 1, (0, 255, 255), 4)
+            cv2.circle(img_raw, (b[9], b[10]), 1, (255, 0, 255), 4)
+            cv2.circle(img_raw, (b[11], b[12]), 1, (0, 255, 0), 4)
+            cv2.circle(img_raw, (b[13], b[14]), 1, (255, 0, 0), 4)
+
+        img_raw = Image.fromarray( img_raw[:,:,::-1].astype('uint8')  )
+
+        return img_raw
+
+
+
---
+++ readme.txt
@@ -0,0 +1,28 @@
+The code is based on https://github.com/biubug6/Pytorch_Retinaface
+Their code is pretty easy to use and understand. 
+
+First download ckpt from their github, and put it in ./weights
+
+I added a new script called inference.py, which is based on test_widerface.py and I removed 
+some unnecessary components. 
+I also added a script called detector.py which is based on inference.py and I wraper their code 
+into a class and make this class takes in PIL.Image as input. so you can import this class from outside 
+You can check use_detector.ipynb for usage
+
+I have only tried resnet50 backbone
+
+
+"dets" right above the save image in inference.py is the output information for each image 
+it is a numpy with the shape of 1*15 (I only tested on image with only one face, according to 
+their code, I am sure that the 1 means number of face)
+
+For the 15, it means: box(4) + score(1) + landmarks(5*2) = 15 
+
+Order is: 
+
+Box (xyxy)
+Score
+RightEye(x,y) LeftEye(x,y) Nose(x,y) RightMouse(x,y) LeftMouse(x,y)
+
+
+
